cbff707b11f504ab96bb0f21b6a8d9b657e65d49
Aziz Rahman Yarzai <azizulrahman.yarzai@gmail.com>
2022-09-13 18:01:01 +0430

    13 Sep

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from django.db import models
 
 # Create your models here.
@@ -5,6 +6,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(unique=True, max_length=150)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    is_available = models.BooleanField(default=True)
     
 
 
