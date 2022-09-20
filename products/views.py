b92ff116b65ab58a67424be46b8d343871243f70
Aziz Rahman Yarzai <azizulrahman.yarzai@gmail.com>
2022-09-20 18:04:59 +0430

    Sept 22

--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
+from math import prod
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from products.models import Product
+
+import decimal
 
 
 # Create your views here.
@@ -21,11 +24,32 @@ def list_products(request):
 def create_product(request):
     if request.method == "GET":
         return render(request, 'products/create-product.html')
+    else:
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        is_available = request.POST.get('is_available')
+        category = request.POST.get('category')
+        desc = request.POST.get('desc')
+
+        product = Product.objects.create(
+            name=name, price=price,
+            is_available=bool(is_available), 
+            category=category,
+            description=desc
+        )
+
+        product.price = float(product.price) +  100
+        product.save()
+
+
+        return HttpResponse(f"Created id - {product.id} - {product.created}")
 
 
 # update product
 def update_product(request, prod_id):
-    pass
+    if request.method == 'GET':
+        product = Product.objects.get(id=prod_id)
+        return render(request, 'products/update-product.html', {'product': product})
 
 # delete product
 def delete_product(request, prod_id):
