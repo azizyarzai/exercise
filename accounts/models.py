9126cfb8b197cc8e31b7726700c130d650d2cfc4
azizyarzai <azizulrahman.yarzai@gmail.com>
2022-10-27 18:02:06 +0430

    Oct 27

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from enum import unique
 from django.db import models
 from django.contrib.auth.models import (
@@ -16,15 +17,13 @@ ROLES = [
 
 class AccountManager(BaseUserManager):
 
-    def create_user(self, email, name, role, date_of_birth=None, password=None):
+    def create_user(self, email, name, date_of_birth=None, password=None):
         if not email:
             raise ValueError("Email is required")
         if not name:
             raise ValueError("Name is required")
-        if not role:
-            raise ValueError("Role is required")
 
-        user = self.model(name=name, email=email, role=role,
+        user = self.model(name=name, email=email,
                           date_of_birth=date_of_birth)
 
         user.set_password(password)
@@ -32,9 +31,9 @@ class AccountManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, name, role, date_of_birth=None, password=None):
+    def create_superuser(self, email, name, date_of_birth=None, password=None):
         user = self.create_user(
-            email=email, name=name, date_of_birth=date_of_birth, password=password, role=role)
+            email=email, name=name, date_of_birth=date_of_birth, password=password)
 
         user.is_superuser = True
         user.is_staff = True
@@ -46,7 +45,7 @@ class AccountManager(BaseUserManager):
 class Account(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=150)
-    role = models.CharField(max_length=100, choices=ROLES)
+    role = models.CharField(max_length=100, choices=ROLES, default='developer')
     date_of_birth = models.DateField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -54,7 +53,7 @@ class Account(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
 
-    REQUIRED_FIELDS = ['name', 'role']
+    REQUIRED_FIELDS = ['name']
 
     objects = AccountManager()
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from email import message
+import re
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+User = get_user_model()
+
 
 # Create your views here.
+
+
+def register_user(request):
+    if request.user.is_authenticated:
+        return redirect('products:list')
+    if request.method == "GET":
+        return render(request, 'accounts/register.html')
+
+    else:
+        email = request.POST.get("email")
+        name = request.POST.get("name")
+        pass1 = request.POST.get("pass1")
+        pass2 = request.POST.get("pass2")
+
+        if pass1 != pass2:
+            messages.error(request, "Password does not match.")
+            return redirect('accounts:register')
+
+        exists = User.objects.filter(email=email).exists()
+        if exists:
+            messages.error(request, 'Email already exist.')
+            return redirect('accounts:register')
+
+        user = User.objects.create_user(name=name, email=email, password=pass1)
+        messages.success(request, 'User was created successfully.')
+        return redirect("products:list")
+
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('products:list')
+    if request.method == "GET":
+        return render(request, 'accounts/login.html')
+
+    else:
+        email = request.POST.get("email")
+        pass1 = request.POST.get("pass1")
+
+        user = authenticate(request, email=email, password=pass1)
+        if not user:
+            messages.error(request, 'Wrong credentials.')
+            return redirect("accounts:login")
+
+        login(request, user)
+        messages.success(request, 'You are logged in successfully.')
+        return redirect("products:list")
+
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return redirect("products:list")
